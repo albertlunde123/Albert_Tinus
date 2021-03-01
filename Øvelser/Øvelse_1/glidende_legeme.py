8398205edff7a530fd4f49a6762c06d1615e8113
albertlunde123 <albert.lunde@gmail.com>
2021-03-01 11:51:14 +0100

    Jeg har lavet et glidende_legeme dokument

---
+++ Øvelser/Øvelse_1/glidende_legeme.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+
+fig, ax = plt.subplots()
+
+exec(open('kalibrering.py').read())
+exec(open('data_renser.py').read())
+exec(open('Statistik.py').read())
+
+data = Data('Messing')
+
+x = func(sol1.points, *popt)
+t = 1000 * sol1.t
+
+mask = sol1.rinse([[-1, 0.1], [0.4, 0.3], [0.6, 0.4]])
+
+ax.scatter(t[~mask], x[~mask], color = 'blue', label = 'outliers')
+ax.scatter(t[mask], x[mask], color = 'red', label = 'data points')
+
+
+
+def func(t,*p):
+    a = p[0]
+    t0 = p[1]
+    c = p[2]
+    b = p[3]
+    return (1/2*a*(t-t0)**2+b*(t)+c)
+
+guess_params = [1,1,1,1]
+
+popt,pcov = scp.curve_fit(func, t[mask], x[mask], guess_params)
+
+t_fit = np.linspace(-0.4,1.0,1000)
+
+ax.plot(t_fit, func(t_fit, *popt), color = 'k', linewidth = 3,
+        label = 'fitted function')
+
+ax.set_ylim(-0.2,0.7)
+ax.set_ylabel('x/m')
+ax.set_xlabel('t/s')
+ax.legend()
+
+plt.show()
+
+###############
+
+grader = 15
+theta = grader*(2*np.pi/360)
+
+a = [np.sin(theta)*9.82*0.66, popt[0]]
+print(a)
+
+
+
+# fig.savefig('ruller.png')
+
