8d5a8c182d82a5960be13829a22832418a39c6d3
albertlunde123 <albert.lunde@gmail.com>
2021-04-15 11:03:31 +0200

    forbedret anim

--- Øvelser/Øvelse_3/animate_puk.py
+++ Øvelser/Øvelse_3/animate_puk.py
@@ -4,21 +4,25 @@ from matplotlib import animation
 
 fig, ax = plt.subplots(figsize = (16,8))
 
-Rota_Kastet = puk.Puk(['Rota/KastetCenter','Rota/KastetSide'], 1, 1)
-Rota_Stille = puk.Puk(['Rota/StilleCenter','Rota/StilleSide'], 1, 1)
+Rota_Kastet = puk.Puk(['Uelastisk/KastetCenter','Uelastisk/KastetSide'], 1, 1)
+Rota_Stille = puk.Puk(['Uelastisk/StilleCenter','Uelastisk/StilleSide'], 1, 1)
 puks = [Rota_Kastet, Rota_Stille]
 
 def pukanim(puks, ax):
     N = len(puks[0].get_center(0))
-    color = ['bo', 'ro']
+    color = ['bo', 'ro', 'ko', 'ko']
 
-    puk1 = ax.plot(puks[0].get_center(1)[0], puks[0].get_center(2)[0], color[0], label = 'Puk distance', ms = 75)[0]
-    puk2 = ax.plot(puks[1].get_center(1)[0], puks[1].get_center(2)[0], color[1], label = 'Puk distance', ms = 75)[0]
+    puk11 = ax.plot(puks[0].get_center(1)[0], puks[0].get_center(2)[0], color[0], label = 'Puk distance', ms = 75)[0]
+    puk21 = ax.plot(puks[1].get_center(1)[0], puks[1].get_center(2)[0], color[1], label = 'Puk distance', ms = 75)[0]
+    puk12 = ax.plot(puks[0].get_edge(1)[0], puks[0].get_edge(2)[0], color[2], label = 'Puk distance', ms = 20)[0]
+    puk22 = ax.plot(puks[1].get_edge(1)[0], puks[1].get_edge(2)[0], color[3], label = 'Puk distance', ms = 20)[0]
 
     def update(i):
-        puk1.set_data(puks[0].get_center(1)[i], puks[0].get_center(2)[i])
-        puk2.set_data(puks[1].get_center(1)[i], puks[1].get_center(2)[i])
-        return puk1, puk2
+        puk11.set_data(puks[0].get_center(1)[i], puks[0].get_center(2)[i])
+        puk21.set_data(puks[1].get_center(1)[i], puks[1].get_center(2)[i])
+        puk12.set_data(puks[0].get_edge(1)[i], puks[0].get_edge(2)[i])
+        puk22.set_data(puks[1].get_edge(1)[i], puks[1].get_edge(2)[i])
+        return puk11, puk21, puk22, puk12
 
     anim = animation.FuncAnimation(fig,
                                 update,
