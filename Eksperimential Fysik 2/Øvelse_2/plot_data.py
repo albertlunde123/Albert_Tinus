c4a3da0033b2714dbb39ae4b9e025dacd7a76c1b
albertlunde123 <albert.lunde@gmail.com>
2022-03-10 16:09:51 +0100

    ll

---
+++ Eksperimential Fysik 2/Øvelse_2/plot_data.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import scipy.optimize as scp
+
+fig, ax = plt.subplots()
+
+os.chdir('c:\\Users\\all\\Albert_Tinus\\Eksperimential Fysik 2\\Øvelse_2\\')
+
+data = np.loadtxt("Data2øvelsesgang/Måling1.txt", skiprows = 3)
+
+def sorter(data):
+    returner = []
+    for i in range(len(data)-1):
+        if data[i] > data[i+1]:
+            returner.append(i)
+    return returner
+
+def fit(V, *p):
+    a = p[0]
+    b = p[1]
+    c = p[2]
+    d = p[3]
+    k = p[4]
+    # e = p[4]
+    return a*np.cos(b + V*d * np.exp((-k/800)*V)) + c
+
+indices = sorter(data[:, 1])
+
+freq = data[indices, 1]*10
+voltage = data[indices, 2]
+
+maks = np.max(voltage[:200])
+min = np.min(voltage[:200])
+
+
+guess_a = (maks - min)/2
+guess_c = (maks + min)/2
+
+def plot(freq, voltage, ax):
+    ax.plot(freq, voltage, 'o', color = 'blue')
+
+guess_d = 0.4
+
+guess_params = [guess_a, -2.1, guess_c, guess_d, 1.03]
+
+
+popt, pcov = scp.curve_fit(fit,
+                    freq,
+                    voltage,
+                    guess_params,
+                    bounds = ((guess_a-1, -2.11, guess_c-1, guess_d - 0.1, 1.02), (guess_a+1, -2.09, guess_c+1, guess_d + 0.1, 1.04)))
+
+Vs = np.linspace(0, 100, 100)
+
+# ax.plot(Vs, fit(Vs, *popt), '-')
+# plot(freq, voltage, ax)
+
+print(popt)
+# in theory, we should be able to extract the path distance from this.
+
+wave_length = 700*10**-3
+
+def path_dist(V, *p):
+    a = p[0]
+    k = p[1]
+    return a*V*np.exp(-k*V)
+
+ax.plot(Vs, path_dist(Vs, *[0.4142, 0.00102])*wave_length/(4*3.14))
+
+
+plt.show()
